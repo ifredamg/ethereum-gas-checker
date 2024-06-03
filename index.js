37566d228d4e65530c95671e4a9978c4c0ca8492
Frederico Santos <fsraen@gmail.com>
2024-06-03 13:17:46 +0100

    Implement ratings system for classification.

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ require('dotenv').config();
 const axios = require('axios');
 const nodemailer = require('nodemailer');
 const cron = require('node-cron');
+const canISendServiceStartEmail = true;
 const intervalInMinutes = 60;
+
+const configRatings = [
+    {
+        code: "LOW",
+        emailSubject: "The cost of gas on the Ethereum network is low!",
+        maxPrice: 10,
+        sendEmail: true
+    },
+    {
+        code: "MEDIUM",
+        emailSubject: "The cost of gas on the Ethereum network is medium!",
+        minPrice: 10,
+        maxPrice: 20,
+        sendEmail: true
+    },
+    {
+        code: "HIGHT",
+        emailSubject: "The cost of gas on the Ethereum network is high!",
+        minPrice: 20,
+        maxPrice: 30,
+        sendEmail: true
+    },
+    {
+        code: "SUPER_HIGH",
+        emailSubject: "The cost of gas on the Ethereum network is insane!",
+        minPrice: 30,
+        sendEmail: true
+    },
+];
 
 async function getGasPrice() {
     try {
+        logInfo('Checking gas cost...');
         const response = await axios.get(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETHERSCAN_API_KEY}`);
         const gasPrice = response.data.result.ProposeGasPrice;
         return gasPrice;
@@ -17,7 +48,7 @@ async function getGasPrice() {
 
 function createEmailTransport() {
     return nodemailer.createTransport({
-        service: 'gmail', 
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_ORIGEM_ENDERECO,
             pass: process.env.EMAIL_ORIGEM_PASSWORD
@@ -25,27 +56,44 @@ function createEmailTransport() {
     });
 }
 
+function getConfigRating(gasPrice) {
+    return configRatings.find(config => {
+        return (config.minPrice === undefined || gasPrice >= config.minPrice) &&
+            (config.maxPrice === undefined || gasPrice < config.maxPrice);
+    });
+}
+
 async function sendGasPriceEmail(gasPrice) {
     let transporter = createEmailTransport();
+    let configRating = getConfigRating(gasPrice);
 
     let mailOptions = {
         from: process.env.EMAIL_ORIGEM_ENDERECO,
         to: process.env.EMAIL_DESTINO_ENDERECO,
-        subject: 'Gas Cost on the Ethereum Network',
+        subject: configRating.emailSubject,
         text: `The current cost of gas on the Ethereum network is ${gasPrice} Gwei.`
     };
 
-    try {
-        let info = await transporter.sendMail(mailOptions);
-        console.log('Email sent: ' + info.response);
-    } catch (error) {
-        console.error('Error sending the e-mail:', error);
+    if (configRating.sendEmail) {
+        try {
+            let info = await transporter.sendMail(mailOptions);
+            logInfo('Email sent: ' + info.response);
+            logInfo(`${configRating.emailSubject} | ${gasPrice} Gwei.`);
+        } catch (error) {
+            console.error('Error sending the e-mail:', error);
+        }
+    }
+    else
+    {
+        logInfo(`The email was skipped. ${configRating.emailSubject} | ${gasPrice} Gwei.`);
     }
 }
 
 async function sendServiceStartEmail() {
-    console.log("The service started successfully!");
-    
+    logInfo("The service started successfully!");
+
+    if(!canISendServiceStartEmail)
+        return;
     let transporter = createEmailTransport();
 
     let mailOptions = {
@@ -57,7 +105,7 @@ async function sendServiceStartEmail() {
 
     try {
         let info = await transporter.sendMail(mailOptions);
-        console.log('Email sent: ' + info.response);
+        logInfo('Email sent: ' + info.response);
     } catch (error) {
         console.error('Error sending the e-mail:', error);
     }
@@ -72,10 +120,13 @@ async function checkGasPriceAndSendEmail() {
     }
 }
 
+function logInfo(info) {
+    console.log(info);
+}
+
 sendServiceStartEmail();
 checkGasPriceAndSendEmail();
 
 cron.schedule(`*/${intervalInMinutes} * * * *`, () => {
-    console.log('Checking gas cost and sending email...');
     checkGasPriceAndSendEmail();
 });
